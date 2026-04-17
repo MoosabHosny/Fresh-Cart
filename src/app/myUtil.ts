563fe534ad9bd532687706664e9fc87f5707d810
unknown <hsnymsb6@gmail.com>
2026-04-17 11:20:01 +0200

    Fix: final token logic for production

--- src/app/myUtil.ts
+++ src/app/myUtil.ts
@@ -1,14 +1,28 @@
+// import { getToken } from "next-auth/jwt";
+// import { cookies } from "next/headers";
+
+// export async function getUserToken() {
+//     const cookieStore = await cookies();
+    
+//     const token = await getToken({
+//         req: {
+//           cookies: cookieStore
+//         } as any,
+//         secret: process.env.NEXTAUTH_SECRET,
+//     });
+//     return token?.token as string | undefined;
+// }
+
+
 import { getToken } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
+
 export async function getUserToken() {
-    const cookieStore = await cookies();
-    
     const token = await getToken({
-        req: { 
-          cookies: cookieStore 
-        } as any,
+        req: { cookies: await cookies() } as any,
         secret: process.env.NEXTAUTH_SECRET,
     });
-    return token?.token as string | undefined;
+
+    return (token?.token || token?.accessToken) as string | undefined;
 }
